7df643b57041da8fb20c20f5268c9eef3a97abdd
Anderson Assis <aabibcl@gmail.com>
2019-07-01 15:46:20 -0300

    Fixed add functions show() in Models

--- example/app/Telephone.php
+++ example/app/Telephone.php
@@ -8,4 +8,8 @@ class Telephone extends Model
 {
     protected $fillable = ['id', 'ddd', 'phone_number', 'id_people'];
     protected $table = 'telephone';
+
+    public function showPeopleTelephone(){
+        return $this->belongsTo(People::class, "id_people");
+    }
 }
